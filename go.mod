bab9dbb2eea1697d8601b5e2776ad16a41bad4a3
Rana Singh Shashwat <ranalmnl@Ranas-Mac.local>
2024-05-02 14:42:34 +0530

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Aasif-Javid/TSS-comm
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	client "github.com/Aasif-Javid/TSS-comm/Client"
+	server "github.com/Aasif-Javid/TSS-comm/Server"
+	// "./server"
+)
+
+func main() {
+	var i int
+	// ip := "127.0.0.1"
+
+	fmt.Println("HELLO")
+	fmt.Scanf("%d", &i)
+	if i == 1 {
+		server.Server()
+	} else {
+		client.Client()
+	}
+}
